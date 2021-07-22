35b576fc1af2355da3256b03ebcccdddf9ff6207
unknown <12yuely>
2021-07-21 20:21:42 -0500

    Proyecto Diplomado Unidad 3

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,121 @@
+
+$(".size").mouseover(function(){
+    $(this).css("font-size" , "30px")
+})
+
+
+$(".box").mouseover(function(){
+    $(this).css("background" , "orange")
+
+})
+
+$(".box").mouseout(function(){
+    $(this).css("background" , "pink")
+})
+
+
+
+$("size").dblclick(function(){
+    $(this).css("font.size", "60px");
+})
+
+$(".box2").dblclick(function(){
+    $(this).css("background" , "blue")
+})
+
+$(".box2").click(function(){
+    $(this).css("background" , "green")
+     
+})
+
+
+
+$(".China").css("color" , "green");
+
+
+$(".size").mouseover(function(){
+    $(".China").css("font-size" , "50px");
+    $(this).css("font-size" , "30px")
+    })
+
+
+
+$(".Singapur").click(function(){
+    $(".Singapur").css("background" , "pink");
+})
+
+$(".size").mouseover(function(){
+    $(".Singapur").css("font-size" , "30px");
+    $(this).css("font-size" , "25px")
+    })
+
+    $(".Macao").css("color" , "red");
+    $(".size").dblclick(function(){
+        $(".Macao").css("font-size" , "20px");
+        $(this).css("font-size" , "40px")
+
+    })
+
+  
+$(".size").mouseout( function(){
+$(".HongKong").css("font-size" , "25px");
+$(this).css("font-size" , "35px")
+})
+
+
+$(".HongKong").dblclick(function(){
+$(".HongKong").css("background" , "yellow");
+})
+
+
+
+
+$(".Estonia").css("color" , "orange");
+
+$(".size").mouseover(function(){
+    $(".Estonia").css("font-size" , "10px");
+    $(this).css("font-size" , "15px")
+    })
+
+
+
+
+$(".Canada").css("color" , "purple");
+
+$(".Canada").click(function(){
+    $(".Canada").css("background" , "gray");
+})
+
+
+$(".Finlandia").css("color" , "red");
+
+$(".size").mouseover(function(){
+    $(".Finlandia").css("font-size" , "80px");
+    $(this).css("font-size" , "50px")
+    })
+
+    $(".size").mouseout( function(){
+        $(".Irlanda").css("font-size" , "38px");
+        $(this).css("font-size" , "50px")
+        })
+
+        $(".Irlanda").css("color" , "brown");
+
+
+        $(".Corea").css("color" , "blue");    
+       
+        $(".Corea").dblclick(function(){
+            $(".Corea").css("background" , "white");
+            })
+
+            $(".size").mouseover(function(){
+                $(".Polonia").css("font-size" , "40px");
+                $(this).css("font-size" , "20px")
+                })  
+                
+                $(".Polonia").click(function(){
+                    $(".Polonia").css("background" , "violet");
+                    })
+        
+
+
